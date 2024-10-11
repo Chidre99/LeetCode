74a34fa5764e11c140faefc578b59a47bf4918d4
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2024-10-11 23:07:46 +0530

    Time: 10 ms (11.18%), Space: 8.4 MB (91.45%) - LeetHub

---
+++ 0674-longest-continuous-increasing-subsequence/0674-longest-continuous-increasing-subsequence.c
@@ -0,0 +1,23 @@
+int findLengthOfLCIS(int* nums, int numsSize) {
+    if(numsSize == 0) return 0;
+    int count = 1;
+    int maxLength = 1;
+    for(int i = 1; i < numsSize; i++){
+        if(nums[i-1] < nums[i]){
+            count++;
+            if(count > maxLength){
+                maxLength = count;
+            }
+        }else if(nums[i-1] >= nums[i]){
+            if(count > maxLength){
+                maxLength = count;
+            }
+            count = 1;
+        }
+        if(count > maxLength){
+            maxLength = count;
+        }
+        
+    }
+    return maxLength;
+}
