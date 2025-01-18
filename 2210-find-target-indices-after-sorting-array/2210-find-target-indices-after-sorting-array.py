525ead9513aa5cd7aa6796be107f6d871ef25c86
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-01-18 11:49:24 +0530

    Time: 0 ms (100%), Space: 17.8 MB (21.09%) - LeetHub

---
+++ 2210-find-target-indices-after-sorting-array/2210-find-target-indices-after-sorting-array.py
@@ -0,0 +1,31 @@
+from typing import List
+
+class Solution:
+    def targetIndices(self, nums: List[int], target: int) -> List[int]:
+        nums.sort()
+        result = []
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        start = left
+
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = left + (right - left) // 2
+            if nums[mid] <= target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        end = right
+
+        if start <= end:
+            result = list(range(start, end + 1))
+
+        return result
