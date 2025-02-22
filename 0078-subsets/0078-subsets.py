9d7f474781d500d38afae8197770996cfb34dc72
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-02-22 19:32:53 +0530

    Time: 0 ms (100%), Space: 18 MB (40.38%) - LeetHub

--- 0078-subsets/0078-subsets.py
+++ 0078-subsets/0078-subsets.py
@@ -1,20 +1,10 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        result = []
-
-        def backtrack(start, current_subset):
-            # Add the current subset to the result
-            result.append(current_subset[:])
-            
-            # Explore further subsets
-            for i in range(start, len(nums)):
-                # Include nums[i] in the current subset
-                current_subset.append(nums[i])
-                # Recurse with the next starting index
-                backtrack(i + 1, current_subset)
-                # Backtrack by removing the last added element
-                current_subset.pop()
-        
-        # Start the backtracking with an empty subset
-        backtrack(0, [])
-        return result
+        if not nums:
+            return [[]]
+    
+        first, rest = nums[0], nums[1:]
+        subsets_without_first = self.subsets(rest)
+        subsets_with_first = [subset + [first] for subset in subsets_without_first]
+    
+        return subsets_without_first + subsets_with_first
