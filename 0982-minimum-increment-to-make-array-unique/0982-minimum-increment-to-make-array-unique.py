9f907de92b82b00edf7a7aa63b014bdd6c82544d
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-06-25 09:12:50 +0530

    Time: 144 ms (57.48%), Space: 28.5 MB (82.14%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.py
@@ -4,7 +4,8 @@ class Solution:
         moves = 0
         for i in range(1,len(nums)):
             if nums[i] <= nums[i-1]:
-                moves = moves - nums[i] + nums[i-1]+1
+                add = - nums[i] + nums[i-1]+1
+                moves = moves + add
                 nums[i] = nums[i-1] + 1
                 
         return moves
