d656ac1f15611fae98d2010c6ef0f7dda2c3941f
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-07-28 11:28:10 +0530

    Time: 7 ms (45.84%), Space: 18.9 MB (64.61%) - LeetHub

---
+++ 0800-letter-case-permutation/0800-letter-case-permutation.py
@@ -0,0 +1,26 @@
+class Solution:
+    def letterCasePermutation(self, s: str) -> List[str]:
+        res = []
+
+        def backtrack(i, path):
+            if i == len(s):
+                res.append("".join(path))
+                return
+
+            if s[i].isdigit():
+                path.append(s[i])
+                backtrack(i + 1, path)
+                path.pop()
+            else:
+                # Lowercase
+                path.append(s[i].lower())
+                backtrack(i + 1, path)
+                path.pop()
+
+                # Uppercase
+                path.append(s[i].upper())
+                backtrack(i + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
