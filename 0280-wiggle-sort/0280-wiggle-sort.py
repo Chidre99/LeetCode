c0ab1f58ae12879bda5f63e3aab216262eff380e
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-07-28 11:25:04 +0530

    Time: 4 ms (41.34%), Space: 18.6 MB (52.14%) - LeetHub

---
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -0,0 +1,9 @@
+class Solution:
+    def wiggleSort(self, nums: List[int]) -> None:
+        for i in range(len(nums) - 1):
+            if i % 2 == 0:
+                if nums[i] > nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            else:
+                if nums[i] < nums[i + 1]:
+                    nums[i], nums[i + 1] = nums[i + 1], nums[i]
