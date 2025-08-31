936131fddca4b0906e42407e59095eac774b0169
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2025-08-31 23:58:54 +0530

    Time: 17 ms (21.63%), Space: 18.6 MB (12.69%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,20 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        # If the lengths of the strings are different, they can't be anagrams
         if len(s) != len(t):
             return False
-        
-        # Use a dictionary to count character occurrences in the first string
-        count = {}
-        
-        for char in s:
-            count[char] = count.get(char, 0) + 1
-        
-        # Subtract character occurrences based on the second string
-        for char in t:
-            if char not in count or count[char] == 0:
-                return False
-            count[char] -= 1
-        
-        # If all counts are zero, it's an anagram
-        return all(value == 0 for value in count.values())
+
+        return sorted(s) == sorted(t)
