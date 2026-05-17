38c4a16415ed697509b13515ece2bf0d83805e1d
Aditya Chidre <91117910+Chidre99@users.noreply.github.com>
2026-05-18 01:03:00 +0530

    Time: 10 ms (87.72%), Space: 22 MB (80.18%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,9 +1,9 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagrams = {}
-        for string in strs:
-            key = "".join(sorted(string))
-            if key not in anagrams:
-                anagrams[key] = []
-            anagrams[key].append(string)
-        return list(anagrams.values())
+        groups = defaultdict(list)
+
+        for word in strs:
+            key = ''.join(sorted(word))
+            groups[key].append(word)
+        
+        return list(groups.values())
